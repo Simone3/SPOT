f820852635cd5656d7c8e2d94c25b71d3444acc0
Simone <simone.graziussi@gmail.com>
2024-11-09 18:25:55 +0100

    Fix indentations and ESLint warnings

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
-const path = require('node:path')
+const { app, BrowserWindow, ipcMain } = require('electron/main');
+const path = require('node:path');
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js')
+		}
+	});
 
-  // TODO https://www.electronjs.org/docs/latest/tutorial/security#18-avoid-usage-of-the-file-protocol-and-prefer-usage-of-custom-protocols
-  const startURL = 'http://localhost:3000'
-  // `file://${path.join(__dirname, '../build/index.html')}`;
+	// TODO https://www.electronjs.org/docs/latest/tutorial/security#18-avoid-usage-of-the-file-protocol-and-prefer-usage-of-custom-protocols
+	// `file://${path.join(__dirname, '../build/index.html')}`;
+	const startURL = 'http://localhost:3000';
 
-  win.loadURL(startURL);
-}
+	win.loadURL(startURL);
+};
 
 app.whenReady().then(() => {
-  ipcMain.handle('ping', () => 'pong')
+	ipcMain.handle('ping', () => 'pong');
 
-  createWindow()
+	createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+	app.on('activate', () => {
+		if(BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+	if(process.platform !== 'darwin') {
+		app.quit();
+	}
+});
--- renderer.js
+++ renderer.js
@@ -1,11 +1,9 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+const information = document.getElementById('info');
+information.innerText = `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${window.versions.node()}), and Electron (v${window.versions.electron()})`;
 
-const func = async () => {
-	const response = await window.versions.ping()
-	information.innerText = information.innerText + ' ' + response;
-	console.log('here we are!'); // Command + Option + i
-}
+const func = async() => {
+	const response = await window.versions.ping();
+	console.log(response);
+};
 
-func()
-
+func();
