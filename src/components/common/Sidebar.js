7bab2de8f574fd76fb619121f645ffc95fffb4ae
Simone <simone.graziussi@gmail.com>
2024-11-29 22:32:58 +0100

    Sidebar icons

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -1,11 +1,21 @@
 import './Sidebar.css';
 import { NavLink } from 'react-router';
+import NotesIcon from '../icons/NotesIcon';
+import SettingsIcon from '../icons/SettingsIcon';
+import TagsIcon from '../icons/TagsIcon';
+import TasksIcon from '../icons/TasksIcon';
 
 const Sidebar = () => {
 	return (
 		<div className='sidebar'>
-			<NavLink to='/'>Tasks</NavLink>
-			<NavLink to='/notes'>Notes</NavLink>
+			<div className='top'>
+				<NavLink to='/'><TasksIcon/></NavLink>
+				<NavLink to='/notes'><NotesIcon/></NavLink>
+				<NavLink to='/tags'><TagsIcon/></NavLink>
+			</div>
+			<div className='bottom'>
+				<NavLink to='/settings'><SettingsIcon/></NavLink>
+			</div>
 		</div>
 	);
 };
